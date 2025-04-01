77e54988fe48a5b829bd5e83799b98eff751a9e1
Bruce Filippone <brfi5663@colorado.edu>
2025-04-01 11:02:37 -0600

    created database

---
+++ ProjectSourceCode/src/init_data/create.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS Users CASCADE;
+CREATE TABLE IF NOT EXISTS Users (
+  user_id SERIAL PRIMARY KEY NOT NULL,
+  rewards_total INT,
+  username VARCHAR(60) NOT NULL,
+  email VARCHAR(60) NOT NULL,
+  password_hashed VARCHAR(60) NOT NULL
+);
+
+DROP TABLE IF EXISTS Tasks CASCADE;
+CREATE TABLE IF NOT EXISTS Tasks (
+  task_id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(100),
+  description VARCHAR(200),
+  rewards INT,
+  due_date DATETIME,
+  FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE
+);
